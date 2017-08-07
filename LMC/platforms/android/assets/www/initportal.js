369cd5d87e87540f8d38611c8ac628ef97ce5c18
Kai Christenson <CoffeeGoon@users.noreply.github.com>
2017-08-06 21:02:27 -0700

    almost done
    needs resizing of inputs

--- LMC/platforms/android/assets/www/initportal.js
+++ LMC/platforms/android/assets/www/initportal.js
@@ -9,10 +9,35 @@ var requests = new Array();
 var requeststo = new Array();
 var active;
 var name;
-
+var reqname = "";
+var namrg;
 var tokens = new Array(3);
+ var users = new Array();
+ var descripts = new Array();
+ var fill = function(swit){
+
+   $("#popdesc" + swit).toggle();
+
+
+ };
+var request = function(num){
+          //alert("poop");
+          var acname = users[num];
+          var descript = descripts[num];
+          reqname = tokens[2] + "requests_" + tokens[0];
+           var stri = "http://lmc.konghexdev.x10host.com/addRequest.php?callback=?&user=" + reqname + "&otheruser=" + acname +"&des=" + descript;
+           //alert(stri);
+         $.getJSON(stri, function(t){
+          document.location.reload();
+           });
+
+         };
+
+
+
 
 var inflate = function(e){
+  var reqname = e.requests;
 	if(e.enable != '1'){
       $('#innards').remove();
     $('#deny').show();
@@ -25,46 +50,155 @@ else{
  //<td id="contactinfo"></td><td id="setvisibility"></td><td id="pagedescription"></td><td id="internalrequests"></td><td id="externalrequests">
 //create onclick listeners for td to fill in result space
 $('#contactinfo').click( function(x){
-  output.innerHTML=  e.contactinfo;
+  $("#lis").hide().listview("refresh");
+  output.innerHTML=  e.contactinfo + "<br>Change: <input id='conchange' type='text'> </input> <br> <div id='consub'>submit</div>";
+  $("#consub").click(function(m){
+    //alert($('#conchange').val());
+    var tname = 'venue_account';
+    if(tokens[2] == 'band'){ tname = 'band_account'; }
+    $.getJSON('http://lmc.konghexdev.x10host.com/changecontact.php?callback=?', {info : $('#conchange').val(), ty : tname, acname : tokens[0] } ,
+        function(g){
+          window.location.reload();
+        }    
+      );
+  });
 });	
 $('#setvisibility').click( function(x){
+  $("#lis").hide().listview("refresh");
   if(e.active == 'false'){
-      output.innerHTML = tes  + " is currently disabled would you like to set your account to active? <br> <button id='atog'>toggle </button>";
+      output.innerHTML = tes  + " is currently disabled would you like to set your account to active? <br> <div style='color:white' id='atog'>toggle </div>";
+   $("#atog").click(function(k){
+    if(tokens[2] == 'venue'){
+      $("#activeinfo").show();
+      $("#actpost").click(function(r){
+      var nuactive = document.getElementById("month").value + " " + document.getElementById("day").value + " " + document.getElementById("year").value + " " + document.getElementById("time").value;
+      var url = "http://lmc.konghexdev.x10host.com/toggleactive.php?callback=?&typ=venue&user=" + tokens[0] + "&value=" + nuactive;
+      $.getJSON(url, function(z){ window.location.reload();});
+    });
+    }
+    else{
+        var url =  "http://lmc.konghexdev.x10host.com/toggleactive.php?callback=?&typ=band&user=" + tokens[0] + "&value=true";
+    $.getJSON(url, function(z){window.location.reload();});
+
+    }
+ });
+
   }
   else{
-    output.innerHTML = tes + " is currently active and visible to other users would you like to deactivate your visibility? <br> <button id='atog'>toggle </button>";
+    output.innerHTML = tes + " is currently active and visible to other users would you like to deactivate your visibility? <br> <div style='color:white' id='atog'>toggle </div>";
+     $("#atog").click( function(k){
+         var url =  "http://lmc.konghexdev.x10host.com/toggleactive.php?callback=?&typ="+ tokens[2] + "&user=" + tokens[0] + "&value=false";  
+          $.getJSON(url, function(z){window.location.reload();}); 
+    });
   }
 
 });
+
+$("#crossbutton").click(function(x){
+var url = "http://lmc.konghexdev.x10host.com/externalRequests.php?callback=?&user=" + tokens[0] + "&type=" + tokens[2];
+var exquests = new Array();
+var req = new Array();
+var cinfo = new Array();
+ $.getJSON(url, function(exr){  exquests = exr.extrequests.slice(); cinfo = exr.cont.slice(); 
+var urlB = "http://lmc.konghexdev.x10host.com/accountrequests.php?callback=?";
+$.getJSON(urlB, { usrid : tokens[0], type : tokens[2]}, function(k){ req = k.requests.slice();
+var least = req.length;
+
+$("#lis").children().remove();
+for(var n = 0; n < least; n++){
+  var stringA = req[n];
+for(var r = 0; r < exquests.length; r++){
+ var stringB = exquests[r];
+ if(stringA == stringB){
+  var item = "<li data-role='list-divider'>" + stringA + " contact info: " +  cinfo[r] + "</li>";
+    $('#lis').append(item).show().listview('refresh');
+ }
+}
+}
+});
+});
+
+}
+  );
+
+
+
 $('#pagedescription').click( function(x){
-     output.innerHTML=  e.bio;
+  $("#lis").hide().listview("refresh");
+     output.innerHTML="<div style='text-align:right' id='editp'> Edit </div>" + e.bio + "<br> <textarea id='parachange'style='font-family:baus; font-size:28px; font-weight:bold' id='acbio' rows='12' cols='50'>" +
+  "</textarea> <br> <div id='changebio' > submit </div> <br>";
+  $('#changebio').hide();
+  $("#parachange").hide();
+  $('#editp').click( function(m){
+     $('#changebio').toggle();
+  $("#parachange").toggle();
+
+  });
+  $('#changebio').click(function(m){
+        //alert($('#parachange').val());
+         var tname = 'venue_account';
+    if(tokens[2] == 'band'){ tname = 'band_account'; }
+    $.getJSON('http://lmc.konghexdev.x10host.com/changebio.php?callback=?', {newbio : $('#parachange').val(), ty : tname, acname : tokens[0] } ,
+        function(g){
+          window.location.reload();
+        }    
+      );     
+
+  });
+
 });	
 $('#internalrequests').click( function(x){
-       var markup = "<br> <button  id='req'>REQUESTS</button> <button id='actchoices'>ACTIVE PROFILES</button> <br> <br>"; 
+  $("#lis").hide().listview("refresh");
+       var markup = "<br><u> <div  id='req'>REQUESTS__</div> <div id='actchoices'>_ACTIVE PROFILES</div> </u> <br> <br>"; 
        output.innerHTML = markup;
      // $("#space").append("<ul id='profiles' data-role='listview' data-inset='true' data-divider-theme='d'> </ul>'");
       // $("#profiles").listview("refresh");
-       var url = "http://127.0.0.1/accountrequests.php?callback=?";
+       var url = "http://lmc.konghexdev.x10host.com/accountrequests.php?callback=?";
        $.getJSON(url, { usrid : tokens[0], type : tokens[2]}, function(k){
-      
+          var ready = 0;         
       $('#actchoices').click(function(){ 
-          $('li').remove();
+          $('#lis').children().remove();
+           $('#lis').show();
+           users = new Array();
+           descripts = new Array();
+           var total = '';
         for( var i = 0; i < k.available.length; i++){
+
           var block = "";
-        for(var j = 0; j < k.available[i].length; j++){
+        for(var j = 0; j < k.available[i].length - 2; j++){
          
             block = block + k.available[i][j] + "  <br>"
       }
-      block = "<li data-role='list-divider'>" + block + "</li>"; 
-     // alert(block);
+
+        namrg = k.available[i][k.available[i].length - 1];
+       users.push(namrg);
+       var changepop =  "<h3 style='background-color:#000000; color:white; font-family:arial' hidden  id='popdesc" + i + "' data-role='listdivider' >  " + k.available[i][k.available[i].length - 2] + "  </h3> ";
+        // $('#popdesc').append(changepop);
+       //   $("#popdesc" + i).hide();
+       //total = total + changepop;
+       displayvalue = k.available[i][0];
+       if(k.available[i].length > 3){ displayvalue = displayvalue + " " + k.available[i][1];}
+       descripts.push(displayvalue);
+      block ="<li  data-role='list-divider'> <a href='' onClick='fill(" + i + ")' >"  + block + "</a>  <li onClick='request(" + i + ")' id='Item" +  i + "'>Request </li> </li>" + changepop ; 
+     // alert(block); onclick='request('" + namrg + "','" +  displayvalue +"'')'
         $('#lis').append(block).listview("refresh");
+        $("#popdesc" + i).hide();
+         
+        //$('#Item').children().last().click( request(namrg, displayvalue) );
+           //var sel = "#Item" + i ;
+      
       }
+    
+  //$('#Item0').click(alert(users[0]));
+      
     });
+     
+
 
       $('#req').click(function(){
-        $('li').remove();
+        $('#lis').children().remove();
+         $('#lis').show();
         for( var i = 0; i < k.requests.length; i++){
-        
         $('#lis').append( "<li data-role='list-divider'>" + k.requests[i] + "</li>").listview("refresh");
       }
 
@@ -78,7 +212,16 @@ $('#internalrequests').click( function(x){
       //list views
 });	
 $('#externalrequests').click( function(x){
-
+  output.innerHTML = "";
+  $('li').remove();
+  $('#lis').show();
+ var url = "http://lmc.konghexdev.x10host.com/externalrequests.php?callback=?&user=" + tokens[0] + "&type=" + tokens[2];
+ $.getJSON(url, function(exr){
+   for(var n = 0; n < exr.extrequests.length; n++){
+     var tok = "<li data-role='list-divider'>" + exr.extrequests[n] + "</li>";
+     $('#lis').append(tok).listview("refresh");
+   }
+ });
     //populate with all opposite entries that have requested this account all active accounts->requested_id->name = e.name
 });
 
@@ -95,11 +238,11 @@ for(var i = 0; i < vars.length; i++){
 }
 
 if(tokens[2] == "band"){
-var url = 'http://127.0.0.1/bandrecord.php?callback=?&username=' + tokens[0] + "&password=" + tokens[1];
+var url = 'http://lmc.konghexdev.x10host.com/bandrecord.php?callback=?&username=' + tokens[0] + "&password=" + tokens[1];
 	$.getJSON(url, inflate);
 }
 else{
-   var url = 'http://127.0.0.1/venuerecord.php?callback=?&username=' + tokens[0] + "&password=" + tokens[1];
+   var url = 'http://lmc.konghexdev.x10host.com/venuerecord.php?callback=?&username=' + tokens[0] + "&password=" + tokens[1];
 	$.getJSON(url, inflate);
 }
 
