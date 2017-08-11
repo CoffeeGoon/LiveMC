99220e8d593dd7f4a6b607acdb399457874478a3
Kai Christenson <CoffeeGoon@users.noreply.github.com>
2017-08-10 18:36:05 -0700

    hehe

--- LMC/platforms/android/assets/www/initportal.js
+++ LMC/platforms/android/assets/www/initportal.js
@@ -16,9 +16,10 @@ var tokens = new Array(3);
  var descripts = new Array();
  
  var del = function(i){
+  alert(i);
   var username = requests(i);
   var call = "http://lmc.konghexdev.x10host.com/deleterequest.php?callback=?";
-  $.getJSON(call, { user : tokens[0], type : tokens[2], otheruser : username}, function(){
+  $.getJSON(call, { user : tokens[0], type : tokens[2], otheruser : username}, function(g){
      window.location.reload();
 
   }
@@ -223,7 +224,7 @@ $('#internalrequests').click( function(x){
          $('#lis').show();
          requests = k.requests.slice();
         for( var i = 0; i < k.requests.length; i++){
-        $('#lis').append( "<li data-role='list-divider'>"  +  decodeURIComponent(k.requests[i]) + "  <img src='img/exx.jpg' onClick='del(" + i + ")'></img> </li>").listview("refresh");
+        $('#lis').append( "<li data-role='list-divider'>  "  +  decodeURIComponent(k.requests[i]) + "  <img src='img/exx.jpg' onClick='del(" + i + ")'></img> </li>").listview("refresh");
       }
 
       });
